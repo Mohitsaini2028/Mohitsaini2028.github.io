eae31583521fe271ddaf6f65cf1b7de1a91f3150
Mohit Saini <mohitsaini00720@gmail.com>
2021-07-21 18:50:04 +0530

    Update settings.py

--- MyBlog/icoder/settings.py
+++ MyBlog/icoder/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gkew&00%kgvl#b9bsusc3+#80fm2lw0!e56!g4)pzai65@v3(y'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
