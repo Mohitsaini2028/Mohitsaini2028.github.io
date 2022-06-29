59705f6f639155686a8b1117574b1691d28bece3
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2022-06-29 13:27:05 +0530

    Delete MyBlog directory

--- MyBlog/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from blog.models import Post, BlogComment
-
-admin.site.register((BlogComment))
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    class Media:
-        js= ('tinyInject.js',)
--- MyBlog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-     path('postComment', views.postComment, name="postComment"),
-    path('', views.blogHome, name="bloghome"),
-    path('<str:slug>', views.blogPost, name="blogPost"),
-]
